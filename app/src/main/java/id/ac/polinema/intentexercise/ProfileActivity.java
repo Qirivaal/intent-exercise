312c843f9d6285513feb0503c1376291aa9036f4
Qirivaal <59521786+Qirivaal@users.noreply.github.com>
2022-12-19 22:57:20 +0700

    beres

--- app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/ProfileActivity.java
@@ -2,13 +2,46 @@ package id.ac.polinema.intentexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView fullname;
+    private TextView email;
+    private TextView homepage;
+    private TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        fullname = findViewById(R.id.label_fullname);
+        email = findViewById(R.id.label_email);
+        homepage = findViewById(R.id.label_homepage);
+        about = findViewById(R.id.label_about);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            fullname.setText(extras.getString("fullname"));
+            email.setText(extras.getString("email"));
+            homepage.setText(extras.getString("homepage"));
+            about.setText(extras.getString("about"));
+        }
+        else{
+            Toast.makeText(this,"Intent is missing", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void handleHomepage(View view) {
+        String alamat = homepage.getText().toString();
+        String url = "http://www."+ alamat;
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
--- app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
+++ app/src/main/java/id/ac/polinema/intentexercise/RegisterActivity.java
@@ -1,14 +1,96 @@
 package id.ac.polinema.intentexercise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private CircleImageView image_profile;
+    private EditText fullnameInput;
+    private EditText emailInput;
+    private EditText homepageInput;
+    private  EditText aboutInput;
+    public static final String FULLNAME_KEY = "fullname";
+    public static final String EMAIL_KEY = "email";
+    public static final String HOMEPAGE_KEY = "homepage";
+    public static final String ABOUT_KEY = "about";
+
+    private static final String TAG = RegisterActivity.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void handleFoto(View view) {
+        image_profile = findViewById(R.id.image_profile);
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            Toast.makeText(this, "Cancel input image", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (requestCode == GALLERY_REQUEST_CODE) {
+            if (data != null) {
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),
+                            imageUri);
+                    image_profile.setImageBitmap(bitmap);
+                }
+                catch (IOException e){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+    }
+
+    public void handleData(View view) {
+        fullnameInput = findViewById(R.id.text_fullname);
+        emailInput = findViewById(R.id.text_email);
+        homepageInput = findViewById(R.id.text_homepage);
+        aboutInput = findViewById(R.id.text_about);
+
+
+        String fullname = fullnameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String homepage = homepageInput.getText().toString();
+        String about = aboutInput.getText().toString();
+
+        Intent intent = new Intent(RegisterActivity.this, ProfileActivity.class);
+        intent.putExtra(FULLNAME_KEY, fullname);
+        intent.putExtra(EMAIL_KEY, email);
+        intent.putExtra(HOMEPAGE_KEY, homepage);
+        intent.putExtra(ABOUT_KEY, about);
+
+        startActivity(intent);
+
     }
 }
